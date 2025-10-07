dae88c1e784ca43a394c9a089d7c8fc3ad24366a
Ayoola Jesujoba C. <jobaayoola@gmail.com>
2025-10-07 17:49:00 +0100

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -50,8 +50,14 @@ function restartGameFunction() {
 
 endGameBtn.addEventListener("click", endGameFunction);
 
-function endGameFunction() {
-   var win = window.open("", "_self");
-      win.close();
+  function endGameFunction() {
+  var userConfirm = confirm("are you sure");
+  if(userConfirm){
+    window.close();
+  }else{
+    
+  }
+   
+}
        
 }
